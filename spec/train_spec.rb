fb32fa3ba0e75ddc94d767103b305f4b524e2ce7
mala23 <mail@mala23.me>
2014-09-21 17:52:56 +0100

    Created train

---
+++ spec/train_spec.rb
@@ -0,0 +1,17 @@
+# link to the Train class
+require './lib/train'
+
+# i am describing the functionality of a specific class, the train
+
+describe Train do
+
+	# passenger is created and handed to all tests
+	let(:passenger) { Passenger.new }
+	# coach double is created and handed to all tests
+	let(:coach) { double :coach }
+
+	# this is a specific behaviour
+	# that i expect to be present
+
+
+end
