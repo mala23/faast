c01f741087618f28428556728d82b722d199a8e1
mala23 <mail@mala23.me>
2014-09-23 08:08:33 +0100

    Created Station class and spec

---
+++ spec/station_spec.rb
@@ -0,0 +1,10 @@
+# link to Station class
+require './lib/station.rb'
+
+# i am describing the functionality of a specific class, the station
+describe Station do
+	
+	# create station and hand to all tests
+	let(:station) { Station.new }
+
+end
